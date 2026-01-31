f0df2ed8d6a9cb13c98baf2163bdf43dbdf18276
Md Imran Siddiqui <mohdimransid786@gmail.com>
2026-01-31 08:56:41 +0530

    setup the utils to get url shortner

---
+++ app/utlis.py
@@ -0,0 +1,10 @@
+BASE62 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+def encode_base62(num: int) -> str:
+    if num == 0:
+        return BASE62[0]
+    result = []
+    while num > 0:
+        result.append(BASE62[num % 62])
+        num //= 62
+    return ''.join(reversed(result))

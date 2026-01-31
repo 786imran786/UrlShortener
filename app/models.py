574e2b8c234af457a4587c27756a457ae3f7410e
Md Imran Siddiqui <mohdimransid786@gmail.com>
2026-01-31 08:56:12 +0530

    setup the models with table schema

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from datetime import datetime
+from .database import Base
+
+class URL(Base):
+    __tablename__ = "urls"
+
+    id = Column(Integer, primary_key=True, index=True)
+    long_url = Column(Text, nullable=False)
+    short_code = Column(String(10), unique=True, index=True)
+    clicks = Column(Integer, default=0)
+    created_at = Column(DateTime, default=datetime.utcnow)
